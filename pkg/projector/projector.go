fa118b07514f531557a0702913c25b9dfcedef12
pavles6 <pavles6@protonmail.com>
2023-04-23 21:26:05 +0200

    Implement core projector features

---
+++ pkg/projector/projector.go
@@ -0,0 +1,114 @@
+package projector
+
+import (
+	"encoding/json"
+	"os"
+	"path"
+)
+
+type Data struct {
+	Projector map[string]map[string]string `json:"projector"`
+}
+
+type Projector struct {
+	config *Config
+	data   *Data
+}
+
+func CreateProjector(config *Config, data *Data) *Projector {
+	return &Projector{
+		config: config,
+		data:   data,
+	}
+}
+
+func (p *Projector) GetValue(key string) (string, bool) {
+
+	curr := p.config.Pwd
+	prev := ""
+
+	for curr != prev {
+		if dir, ok := p.data.Projector[curr]; ok {
+			if value, ok := dir[key]; ok {
+				return value, true
+			}
+		}
+		prev = curr
+		curr = path.Dir(curr)
+	}
+
+	return "", false
+}
+
+func (p *Projector) GetValues() map[string]string {
+
+	values := map[string]string{}
+	paths := []string{}
+
+	curr := p.config.Pwd
+	prev := ""
+
+	for curr != prev {
+		if _, ok := p.data.Projector[curr]; ok {
+			paths = append(paths, curr)
+		}
+		prev = curr
+		curr = path.Dir(curr)
+	}
+
+	for i := len(paths) - 1; i >= 0; i-- {
+		for k, v := range p.data.Projector[paths[i]] {
+			values[k] = v
+		}
+	}
+
+	return values
+
+}
+func (p *Projector) SetValue(key, value string) {
+	pwd := p.config.Pwd
+
+	if _, ok := p.data.Projector[pwd]; !ok {
+		p.data.Projector[pwd] = map[string]string{}
+
+	}
+
+	p.data.Projector[pwd][key] = value
+}
+
+func (p *Projector) RemoveValue(key string) {
+	delete(p.data.Projector[p.config.Pwd], key)
+}
+
+func defaultProjector(config *Config) *Projector {
+	return &Projector{
+		config: config,
+		data: &Data{
+			Projector: map[string]map[string]string{},
+		},
+	}
+}
+
+func NewProjector(config *Config) *Projector {
+	if _, err := os.Stat(config.Config); err == nil {
+		contents, err := os.ReadFile(config.Config)
+		if err != nil {
+			return defaultProjector(config)
+		}
+
+		var data Data
+
+		err = json.Unmarshal(contents, &data)
+
+		if err != nil {
+			return defaultProjector(config)
+		}
+
+		return &Projector{
+			config: config,
+			data:   &data,
+		}
+	}
+
+	return defaultProjector(config)
+}
