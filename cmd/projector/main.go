f9023588ac550892537a34446ec0063802c5ee63
pavles6 <pavles6@protonmail.com>
2023-04-24 13:44:10 +0200

    Initialize projector

--- cmd/projector/main.go
+++ cmd/projector/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -20,5 +21,32 @@ func main() {
 		log.Fatalf("unable to get config %v", err)
 	}
 
-	fmt.Printf("opts: %+v", config)
+	proj := projector.NewProjector(config)
+
+	if config.Operation == projector.Print {
+		if len(config.Args) == 0 {
+			data := proj.GetValues()
+
+			out, err := json.Marshal(data)
+
+			if err != nil {
+				log.Fatalf("unable to marshal data %v", err)
+			}
+
+			fmt.Printf("%v\n", string(out))
+		} else if value, ok := proj.GetValue(config.Args[0]); ok {
+			fmt.Printf("%v\n", value)
+		}
+
+	}
+
+	if config.Operation == projector.Add {
+		proj.SetValue(config.Args[0], config.Args[1])
+		proj.Save()
+	}
+
+	if config.Operation == projector.Remove {
+		proj.RemoveValue(config.Args[0])
+		proj.Save()
+	}
 }
